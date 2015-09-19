4a43abe0591649ecba91858412a60cb0b4e7e350
TakamitsuYotsuyanagi <yotsu12@jupiter.kanazawa-it.ac.jp>
2015-09-19 17:49:30 +0900

    【環境】xcode6.3.2

---
+++ KIT_HACKATHON/cocoapods.m
@@ -0,0 +1,10 @@
+//
+//  cocoapods.m
+//  KIT_HACKATHON
+//
+//  Created by X-men on 2015/09/19.
+//  Copyright (c) 2015年 TakamitsuYotsuyanagi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
