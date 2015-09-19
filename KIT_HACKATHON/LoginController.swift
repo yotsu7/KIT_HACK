2e96093ebb6bca80474588d5eecdc328f3054dfa
TakamitsuYotsuyanagi <yotsu12@jupiter.kanazawa-it.ac.jp>
2015-09-19 18:25:39 +0900

    【修正】スッキリさせた

--- KIT_HACKATHON/LoginController.swift
+++ KIT_HACKATHON/LoginController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var x = 1
         // Do any additional setup after loading the view, typically from a nib.
     }
 
